1e5196182028e93f8348b0874618e1aab56c79cc
abstractsig <>
2020-07-01 23:29:33 +1200

    update io

--- io_device/Include/io_device.h
+++ io_device/Include/io_device.h
@@ -277,7 +277,7 @@ initialise_io_device (void) {
 	initialise_io (io,&io_i);
 	initialise_io_board (io);
 
-	dev_io.bm = initialise_io_byte_memory (io,&heap_byte_memory,UMM_BLOCK_SIZE_8);
+	dev_io.bm = initialise_io_byte_memory (io,&heap_byte_memory,UMM_BLOCK_SIZE_1N);
 	dev_io.vm = mk_umm_io_value_memory (io,UMM_VALUE_MEMORY_HEAP_SIZE,STVM);
 	register_io_value_memory (dev_io.vm);
 
